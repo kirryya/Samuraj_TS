517f395302bc56b49252acb2ac52f69be0e87d4a
kirryya <kir1987@inbox.ru>
2022-02-23 01:21:40 +0300

    props

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -12,8 +12,8 @@ const MyPosts = () => {
                 <button>Remove</button>
             </div>
             <div className={s.posts}>
-                <Post/>
-                <Post/>
+                <Post message="Hi, how are you?" likeCount={15} />
+                <Post message="It is my first post" likeCount={30} />
             </div>
         </div>
     );
--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-const Post = () => {
+export type PostPropsType = {
+    message: string
+    likeCount: number
+}
+
+const Post = (props: PostPropsType) => {
     return (
         <div className={s.item}>
             <img src="https://shapka-youtube.ru/wp-content/uploads/2021/02/avatarka-dlya-skaypa-dlya-parney.jpg"
                  alt="Avatar"/>
-            post1
+            {props.message}
             <div>
-                <span>Like</span>
+                <span>like</span>
+                <span> {props.likeCount}</span>
             </div>
         </div>
     );
