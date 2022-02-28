6c7879407b9bb2e66bd298a39bc6e447d648970a
kirryya <kir1987@inbox.ru>
2022-02-28 12:11:48 +0300

    22_Route

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ const App = () => {
                 <Navbar/>
                 <div className="app-wrapper-content">
                     <Routes>
-                        <Route path={"/dialog"} element={<Dialogs />}/>
-                        <Route path={"/profile"} element={<Profile />}/>
-                        <Route path={"/new"} element={<Dialogs />}/>
-                        <Route path={"/music"} element={<Dialogs />}/>
-                        <Route path={"/settings"} element={<Dialogs />}/>
+                        <Route path="/profile" element={<Profile/>}/>
+                        <Route path="/dialogs/*" element={<Dialogs/>}/>
+                        <Route path="/news" element={<Dialogs/>}/>
+                        <Route path="/music" element={<Dialogs/>}/>
+                        <Route path="/settings" element={<Dialogs/>}/>
                     </Routes>
                 </div>
             </div>
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import s from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
                 <div className={s.dialog + " " + s.active}>
-                    Dimych
+                    <NavLink to="/dialogs/1">Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrei
+                    <NavLink to="/dialogs/2">Andrei</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sveta
+                    <NavLink to="/dialogs/3">Sveta</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to="/dialogs/4">Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Masha
+                    <NavLink to="/dialogs/5">Masha</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
