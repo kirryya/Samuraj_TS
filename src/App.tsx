c731e1e48ea544b37546fef1fd8918735fe5b484
kirryya <kir1987@inbox.ru>
2022-03-03 10:11:27 +0300

    onClick

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import {StateType} from "./redux/state";
 
 type AppPropsType = {
@@ -13,25 +13,24 @@ type AppPropsType = {
 
 const App = (props: AppPropsType) => {
     return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className="app-wrapper-content">
-                    <Routes>
-                        <Route path="/profile" element={<Profile posts={props.appState.profilePage.posts}/>}/>
-                        <Route path="/dialogs/*" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
-                                                                   messages={props.appState.messagesPage.messages}/>}/>
-                        <Route path="/news" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
-                                                              messages={props.appState.messagesPage.messages}/>}/>
-                        <Route path="/music" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
+        <div className="app-wrapper">
+            <Header/>
+            <Navbar/>
+            <div className="app-wrapper-content">
+                <Routes>
+                    <Route path="/profile" element={<Profile posts={props.appState.profilePage.posts}/>}/>
+                    <Route path="/dialogs/*" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
                                                                messages={props.appState.messagesPage.messages}/>}/>
-                        <Route path="/settings" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
-                                                                  messages={props.appState.messagesPage.messages}/>}/>
-                    </Routes>
-                </div>
+                    <Route path="/news" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
+                                                          messages={props.appState.messagesPage.messages}/>}/>
+                    <Route path="/music" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
+                                                           messages={props.appState.messagesPage.messages}/>}/>
+                    <Route path="/settings" element={<Dialogs dialogs={props.appState.messagesPage.dialogs}
+                                                              messages={props.appState.messagesPage.messages}/>}/>
+                </Routes>
             </div>
-        </BrowserRouter>);
+        </div>
+    );
 }
 
 export default App
