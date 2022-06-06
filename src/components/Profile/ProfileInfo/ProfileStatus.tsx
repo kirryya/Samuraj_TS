86b938e928d55c19a605aadac638e7447c2282d2
kirryya <kir1987@inbox.ru>
2022-06-07 01:23:14 +0300

    useEffect

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -32,9 +32,9 @@ class ProfileStatus extends React.Component<ProfilePropsType> {
 
     componentDidUpdate(prevProps: Readonly<ProfilePropsType>, prevState: Readonly<{}>, snapshot?: any) {
         if (prevProps.status !== this.props.status)
-        this.setState({
-            status: this.props.status
-        })
+            this.setState({
+                status: this.props.status
+            })
     }
 
     render() {
--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 
 type ProfilePropsType = {
     status: string
@@ -9,6 +9,10 @@ export const ProfileStatusWithHooks = (props: ProfilePropsType) => {
 
     const [editMode, setEditMode] = useState<boolean>(false)
     const [status, setStatus] = useState<string>(props.status)
+
+    useEffect(() => {
+        setStatus(props.status)
+    }, [props.status])
 
     const activateEditMode = () => {
         setEditMode(true)
