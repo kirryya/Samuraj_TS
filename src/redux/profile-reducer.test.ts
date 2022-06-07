29679646f4e884c8f7f166a0d7383f3d865ba16c
kirryya <kir1987@inbox.ru>
2022-06-07 16:20:37 +0300

    tdd

---
+++ src/redux/profile-reducer.test.ts
@@ -0,0 +1,24 @@
+import profileReducer, {addPostAC} from "./profile-reducer";
+import {PostsType} from "../components/Profile/MyPosts/MyPosts";
+
+let state = {
+    posts: [
+        {id: 1, message: "Hi, how are you?", likeCount: 15},
+        {id: 2, message: "It is my first post", likeCount: 30}
+    ] as Array<PostsType>,
+    profile: null,
+    status: ""
+}
+
+it("length of posts should be incremented", () => {
+    let action = addPostAC("New post")
+    let newState = profileReducer(state, action)
+    expect(newState.posts.length).toBe(3)
+})
+
+it("message of new post should be correct", () => {
+    let action = addPostAC("New post")
+    let newState = profileReducer(state, action)
+    expect(newState.posts[2].message).toBe("New post")
+})
+
