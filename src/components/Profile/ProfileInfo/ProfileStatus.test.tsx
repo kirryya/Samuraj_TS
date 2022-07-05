13def38083f4cc2f94437407697acac0d8b0141e
kirryya <kir1987@inbox.ru>
2022-07-05 10:13:15 +0300

    react-test-renderer

--- src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.tsx
@@ -1,10 +1,50 @@
 import React from "react";
-import {create} from "react-test-renderer";
 import ProfileStatus from "./ProfileStatus";
+import TestRenderer from 'react-test-renderer';
 
 describe("ProfileStatus Component", () => {
+
     test("Status from props should be in the state", () => {
-        const component = create(<ProfileStatus status={"it-kamasutra.com"} updateStatus={() => {}}/>)
-        expect(component.root.instance.state.status).toBe("it-kamasutra.com")
-    })
+        const mockCallback = jest.fn()
+        const testRenderer = TestRenderer.create(<ProfileStatus status={"it-kamasutra.com"}
+                                                                updateStatus={mockCallback}/>)
+        const testInstance = testRenderer.root.instance
+        expect(testInstance.state.status).toBe("it-kamasutra.com")
+    });
+
+    test("after creation span should be displayed with correct status", () => {
+        const mockCallback = jest.fn()
+        const testRenderer = TestRenderer.create(<ProfileStatus status={"it-kamasutra.com"}
+                                                                updateStatus={mockCallback}/>)
+        const testInstance = testRenderer.root
+        expect(() => testInstance.findByType("span")).not.toBeNull()
+    });
+
+    test("after creation input should be displayed with correct status", () => {
+        const mockCallback = jest.fn()
+        const testRenderer = TestRenderer.create(<ProfileStatus status={"it-kamasutra.com"}
+                                                                updateStatus={mockCallback}/>)
+        const testInstance = testRenderer.root
+        expect(() => testInstance.findByType("input")).toThrow()
+    });
+
+    test("input should be displayed in editMode instead of span", async () => {
+        const mockCallback = jest.fn()
+        const testRenderer = TestRenderer.create(<ProfileStatus status={"it-kamasutra.com"}
+                                                                updateStatus={mockCallback}/>)
+        const testInstance = testRenderer.root
+        const span = await testInstance.findByType("span")
+        span.props.onDoubleClick()
+        const input = await testInstance.findByType("input")
+        expect(input.props.value).toBe("it-kamasutra.com")
+    });
+
+    test("callback should be called", () => {
+        const mockCallback = jest.fn()
+        const testRenderer = TestRenderer.create(<ProfileStatus status={"it-kamasutra.com"}
+                                                                updateStatus={mockCallback}/>)
+        const testInstance = testRenderer.root.instance
+        testInstance.deactivateEditMode()
+        expect(mockCallback.mock.calls.length).toBe(1)
+    });
 })
